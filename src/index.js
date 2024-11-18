b2932e17e1f6f26b9bde86d507e364f4267ffc4c
Simone Schiavo <simoneschiavo@icloud.com>
2024-11-18 19:28:38 +0100

    Add logic for email validation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { passwordValidator } from "./formValidator";
 const name = document.querySelector("#name");
 const nameChecksContainer = document.querySelector(".name.checks-container");
 const email = document.querySelector("#email");
+const emailChecksContainer = document.querySelector(".email.checks-container");
 const password = document.querySelector("#password");
 const passwordChecksContainer = document.querySelector(".password.checks-container");
 
@@ -29,3 +30,34 @@ name.addEventListener("blur", () => {
     };
 });
 
+email.addEventListener("blur", () => {
+    const emailRequiredCheck = document.querySelector(
+      ".email-checks > #required-field"
+    );
+    const emailPatternCheck = document.querySelector(
+      ".email-checks > #wrong-pattern"
+    );
+
+    if (email.validity.valueMissing || email.validity.patternMismatch) {
+        emailChecksContainer.classList.add("visible");
+    } else {
+        if (emailChecksContainer.classList.contains("visible")) {
+            emailChecksContainer.classList.remove("visible");
+        }
+        if (!email.classList.contains("input-validated")) {
+            email.classList.add("input-validated");
+        };
+    };
+
+    if (!email.validity.valueMissing) {
+        emailRequiredCheck.classList.add("validated");
+    } else {
+        emailRequiredCheck.classList.remove("validated");
+    };
+
+    if (email.value && !email.validity.patternMismatch) {
+        emailPatternCheck.classList.add("validated");
+    } else {
+        emailPatternCheck.classList.remove("validated");
+    };
+});
