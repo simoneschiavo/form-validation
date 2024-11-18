f681db1b5946469cf879db4ee932fed61d010e73
Simone Schiavo <simoneschiavo@icloud.com>
2024-11-18 21:07:17 +0100

    Add logic to enable the submit button

--- src/formValidator.js
+++ src/formValidator.js
@@ -1,19 +1,21 @@
 export function passwordValidator(password) {
-    const checks = {
-        hasUppercase: /[A-Z]/.test(password),
-        hasLowercase: /[a-z]/.test(password),
-        hasDigit: /\d/.test(password),
-        hasSpecialChar: /[@$!%&?*€()='^£]/.test(password),
-        isLongEnough: password.length >= 8,
-    };
-    
-    for (let check in checks) {
-        if (checks[check]) {
-            const validatedCheck = document.getElementById(`${check}`);
-            validatedCheck.classList.add("validated");
-        }
-    };
+  const checks = {
+    hasUppercase: /[A-Z]/.test(password),
+    hasLowercase: /[a-z]/.test(password),
+    hasDigit: /\d/.test(password),
+    hasSpecialChar: /[@$!%&?*€()='^£]/.test(password),
+    isLongEnough: password.length >= 8,
+  };
 
-    const allChecksValidated = Object.values(checks).every(value => value === true);
-    return allChecksValidated;
-};
+  for (let check in checks) {
+    if (checks[check]) {
+      const validatedCheck = document.getElementById(`${check}`);
+      validatedCheck.classList.add("validated");
+    }
+  }
+
+  const allChecksValidated = Object.values(checks).every(
+    (value) => value === true
+  );
+  return allChecksValidated;
+}
--- src/index.js
+++ src/index.js
@@ -6,66 +6,82 @@ const nameChecksContainer = document.querySelector(".name.checks-container");
 const email = document.querySelector("#email");
 const emailChecksContainer = document.querySelector(".email.checks-container");
 const password = document.querySelector("#password");
-const passwordChecksContainer = document.querySelector(".password.checks-container");
+const passwordChecksContainer = document.querySelector(
+  ".password.checks-container"
+);
+const submitButton = document.querySelector("form > button");
 
 password.addEventListener("focus", () => {
-    passwordChecksContainer.classList.add("visible");
+  passwordChecksContainer.classList.add("visible");
 });
 
 password.addEventListener("blur", () => {
-    passwordChecksContainer.classList.remove("visible");
+  passwordChecksContainer.classList.remove("visible");
 });
 
 password.addEventListener("keyup", () => {
-    passwordValidator(password.value);
-    if (passwordValidator(password.value)) {
-        if (!password.classList.contains("input-validated")) {
-            password.classList.add("input-validated");
-        };
-    };
+  passwordValidator(password.value);
+  if (passwordValidator(password.value)) {
+    if (!password.classList.contains("input-validated")) {
+      password.classList.add("input-validated");
+      enableButton(submitButton);
+    }
+  }
 });
 
 name.addEventListener("blur", () => {
-    if (name.validity.valueMissing) {
-        nameChecksContainer.classList.add("visible");
-    } else {
-        if (nameChecksContainer.classList.contains("visible")) {
-            nameChecksContainer.classList.remove("visible");
-        };
-        if (!name.classList.contains("input-validated")) {
-            name.classList.add("input-validated");
-        };
-    };
+  if (name.validity.valueMissing) {
+    nameChecksContainer.classList.add("visible");
+  } else {
+    if (nameChecksContainer.classList.contains("visible")) {
+      nameChecksContainer.classList.remove("visible");
+    }
+    if (!name.classList.contains("input-validated")) {
+      name.classList.add("input-validated");
+      enableButton(submitButton);
+    }
+  }
 });
 
 email.addEventListener("blur", () => {
-    const emailRequiredCheck = document.querySelector(
-      ".email-checks > #required-field"
-    );
-    const emailPatternCheck = document.querySelector(
-      ".email-checks > #wrong-pattern"
-    );
+  const emailRequiredCheck = document.querySelector(
+    ".email-checks > #required-field"
+  );
+  const emailPatternCheck = document.querySelector(
+    ".email-checks > #wrong-pattern"
+  );
 
-    if (email.validity.valueMissing || email.validity.patternMismatch) {
-        emailChecksContainer.classList.add("visible");
-    } else {
-        if (emailChecksContainer.classList.contains("visible")) {
-            emailChecksContainer.classList.remove("visible");
-        }
-        if (!email.classList.contains("input-validated")) {
-            email.classList.add("input-validated");
-        };
-    };
+  if (email.validity.valueMissing || email.validity.patternMismatch) {
+    emailChecksContainer.classList.add("visible");
+  } else {
+    if (emailChecksContainer.classList.contains("visible")) {
+      emailChecksContainer.classList.remove("visible");
+    }
+    if (!email.classList.contains("input-validated")) {
+      email.classList.add("input-validated");
+      enableButton(submitButton);
+    }
+  }
 
-    if (!email.validity.valueMissing) {
-        emailRequiredCheck.classList.add("validated");
-    } else {
-        emailRequiredCheck.classList.remove("validated");
-    };
+  if (!email.validity.valueMissing) {
+    emailRequiredCheck.classList.add("validated");
+  } else {
+    emailRequiredCheck.classList.remove("validated");
+  }
 
-    if (email.value && !email.validity.patternMismatch) {
-        emailPatternCheck.classList.add("validated");
-    } else {
-        emailPatternCheck.classList.remove("validated");
-    };
+  if (email.value && !email.validity.patternMismatch) {
+    emailPatternCheck.classList.add("validated");
+  } else {
+    emailPatternCheck.classList.remove("validated");
+  }
 });
+
+function enableButton(button) {
+  if (
+    name.classList.contains("input-validated") &&
+    email.classList.contains("input-validated") &&
+    password.classList.contains("input-validated")
+  ) {
+    button.disabled = false;
+  }
+}
