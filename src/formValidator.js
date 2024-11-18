fdd55c16f92941d60b77fbd3775615f7471f6228
Simone Schiavo <simoneschiavo@icloud.com>
2024-11-18 16:53:21 +0100

    Add the form validator function

---
+++ src/formValidator.js
@@ -0,0 +1,12 @@
+function passwordValidator(password) {
+    const checks = {
+        hasUppercase: /[A-Z]/.test(password);
+        hasLowercase: /[a-z]/.test(password);
+        hasDigit: /\d/.test(password);
+        hasSpecialChar: /[@$!%&?*€]/.test(password);
+        isLongEnough: password.length >= 8;
+    };
+    return checks;
+};
+
+
