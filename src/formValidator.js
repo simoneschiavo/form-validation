d07c45f3c7dbe8abb77eda75dd74cb4244efe8fe
Simone Schiavo <simoneschiavo@icloud.com>
2024-11-18 17:09:06 +0100

    Had the password checks element

--- src/formValidator.js
+++ src/formValidator.js
@@ -3,10 +3,8 @@ function passwordValidator(password) {
         hasUppercase: /[A-Z]/.test(password);
         hasLowercase: /[a-z]/.test(password);
         hasDigit: /\d/.test(password);
-        hasSpecialChar: /[@$!%&?*€]/.test(password);
+        hasSpecialChar: /[@$!%&?*€()='^£]/.test(password);
         isLongEnough: password.length >= 8;
     };
     return checks;
 };
-
-
