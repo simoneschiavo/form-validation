77208e85e343d11ba668f5ed873b9b17f446bea4
Simone Schiavo <simoneschiavo@icloud.com>
2024-11-18 17:17:38 +0100

    Update the password validator function

--- src/formValidator.js
+++ src/formValidator.js
@@ -1,10 +1,16 @@
 function passwordValidator(password) {
     const checks = {
-        hasUppercase: /[A-Z]/.test(password);
-        hasLowercase: /[a-z]/.test(password);
-        hasDigit: /\d/.test(password);
-        hasSpecialChar: /[@$!%&?*€()='^£]/.test(password);
-        isLongEnough: password.length >= 8;
+        hasUppercase: /[A-Z]/.test(password),
+        hasLowercase: /[a-z]/.test(password),
+        hasDigit: /\d/.test(password),
+        hasSpecialChar: /[@$!%&?*€()='^£]/.test(password),
+        isLongEnough: password.length >= 8,
     };
-    return checks;
+    
+    for (let check in checks) {
+        if (checks[check]) {
+            const validatedCheck = document.getElementById(`${check}`);
+            validatedCheck.classList.add("validated");
+        }
+    };
 };
