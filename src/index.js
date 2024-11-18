e9fa7a952e4af962dced9f83247519e4ac419c78
Simone Schiavo <simoneschiavo@icloud.com>
2024-11-18 18:16:43 +0100

    Add validation for the name field

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import "./styles.css";
 import { passwordValidator } from "./formValidator";
 
+const name = document.querySelector("#name");
+const nameChecksContainer = document.querySelector(".name.checks-container");
+const email = document.querySelector("#email");
 const password = document.querySelector("#password");
-const checksContainer = document.querySelector(".checks-container");
+const passwordChecksContainer = document.querySelector(".password.checks-container");
 
 password.addEventListener("focus", () => {
-    checksContainer.classList.add("visible");
+    passwordChecksContainer.classList.add("visible");
 });
 
 password.addEventListener("blur", () => {
-    checksContainer.classList.remove("visible");
+    passwordChecksContainer.classList.remove("visible");
 });
 
 password.addEventListener("keyup", () => {
     passwordValidator(password.value);
-})
+});
+
+name.addEventListener("blur", () => {
+    if (name.validity.valueMissing) {
+        nameChecksContainer.classList.add("visible");
+    } else {
+        if (nameChecksContainer.classList.contains("visible")) {
+            nameChecksContainer.classList.remove("visible");
+        };
+    };
+});
+
